bb527992938caf409bb7f2c49ed4b4e671f986e0
gbale101 <52832937+gbale101@users.noreply.github.com>
2023-11-30 18:34:44 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Particles.js configuration
+    var particlesConfig = {
+        particles: {
+            number: {
+                value: 150,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: ["image", "circle"],
+                image: {
+                    src: "snowflakes.png",
+                    width: 250,
+                    height: 250
+                }
+            },
+            opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 1,
+                    opacity_min: 0,
+                    sync: false
+                }
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 5,
+                    size_min: 0.1,
+                    sync: false
+                }
+            },
+            line_linked: {
+                enable: false
+            },
+            move: {
+                enable: true,
+                speed: 3,
+                direction: "bottom",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse"
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push"
+                },
+                resize: true
+            },
+            modes: {
+                repulse: {
+                    distance: 100,
+                    duration: 0.4
+                },
+                push: {
+                    particles_nb: 4
+                }
+            }
+        },
+        retina_detect: true
+    };
+
+    particlesJS("particles-js", particlesConfig);
+});
